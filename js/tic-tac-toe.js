dca3694193f1c37bc6061849ac16c153489f28c3
Anastasia <mails2nastya@gmail.com>
2022-03-28 20:17:05 +0300

    implemant function to draw fild

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -3,15 +3,20 @@ const h = 148;
 const background = '#14bdac';
 
 function new_item(draw) {
-	return draw.rect(w, h).fill(background);
+    return draw.rect(w, h).fill(background);
 }
 
 function draw_field(draw) {
-	new_item(draw)
-	new_item(draw).move(w + 2, 0)
-	new_item(draw).move(w + 2, 0).move(2 * (w + 2), 0)
+    new_item(draw)
+    new_item(draw).move(w + 2, 0)
+    new_item(draw).move(w + 2, 0).move(2 * (w + 2), 0)
 
-	new_item(draw).move(0, h + 2)
+    new_item(draw).move(0, h + 2)
+    new_item(draw).move(w + 2, h + 2)
+    new_item(draw).move(w + 2, h + 2).move(2 * (w + 2)), 2 * (h + 2)
+    new_item(draw).move(0, h + 2).move(0, 2 * (h + 2))
+    new_item(draw).move(w + 2, h + 2).move(w + 2, 2 * (h + 2))
+    new_item(draw).move(w + 2, h + 2).move(2 * (w + 2), 2 * (h + 2))
 };
 
 
